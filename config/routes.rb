0b8c54f3aba104601f20cf443fb7c2b3d5d4f79e
Maggie O'Hara <mkohara@users.noreply.github.com>
2017-02-25 16:13:55 -0600

    user index

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #user RCAV
+  get "/users", :controller => "users", :action => "index"
 end
